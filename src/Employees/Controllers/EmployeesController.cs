d88799a0298b1b07e4e51ce59450d0255ad1a35a
Laurynas <laurynasmeksraitis@gmail.com>
2017-02-20 00:44:43 +0200

    Initial

---
+++ src/Employees/Controllers/EmployeesController.cs
@@ -0,0 +1,51 @@
+﻿using Employees.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult List(int? id)
+        {
+            EmployeeContext context = HttpContext.RequestServices.GetService(typeof(Employees.Models.EmployeeContext)) as EmployeeContext;
+
+            ViewData["page"] = id;
+            ViewData["prevPage"] = id - 1;
+            ViewData["nextPage"] = id + 1;
+
+            return View("Index", context.GetSomeEmployees(id));
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("ID,Name,LastName,NetWage")] Employee employee)
+        {
+            EmployeeContext context = HttpContext.RequestServices.GetService(typeof(Employees.Models.EmployeeContext)) as EmployeeContext;
+            context.Create(employee);
+
+            return View("Index", context.GetAllEmployees());
+        }
+
+        // GET: Employees/Details/5
+        public IActionResult Details(int? id)
+        {
+            EmployeeContext context = HttpContext.RequestServices.GetService(typeof(Employees.Models.EmployeeContext)) as EmployeeContext;
+
+            return View(context.GetEmployee(id));
+        }
+    }
+}
---
+++ src/Employees/Models/Employee.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees.Models
+{
+    public class EmployeeContext
+    {
+        public string ConnectionString { get; set; }
+
+        public EmployeeContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            List<Employee> list = new List<Employee>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM employees", conn);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new Employee()
+                        {
+                            ID = reader.GetInt32("ID"),
+                            Name = reader.GetString("name"),
+                            LastName = reader.GetString("lastname"),
+                            NetWage = reader.GetDecimal("net_wage"),
+                        });
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        public List<Employee> GetSomeEmployees(int? page)
+        {
+            List<Employee> list = new List<Employee>();
+            int? from = page * 10;
+            int to = 10;
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand($"SELECT * FROM employees LIMIT {from}, {to}", conn);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new Employee()
+                        {
+                            ID = reader.GetInt32("ID"),
+                            Name = reader.GetString("name"),
+                            LastName = reader.GetString("lastname"),
+                            NetWage = reader.GetDecimal("net_wage"),
+                        });
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        public int GetEmployeesNum()
+        {
+            int num = 0;
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT COUNT(*) FROM employees", conn);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        num = reader.GetInt32();
+                    }
+                }
+            }
+
+            return num;
+        }
+
+        public Employee GetEmployee(int? id)
+        {
+            Employee employee = new Employee();
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand($"SELECT * FROM employees WHERE ID={id}", conn);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        employee = new Employee()
+                        {
+                            ID = reader.GetInt32("ID"),
+                            Name = reader.GetString("name"),
+                            LastName = reader.GetString("lastname"),
+                            NetWage = reader.GetDecimal("net_wage"),
+                        };
+                    }
+                }
+            }
+
+            return employee;
+        }
+
+        public void Create(Employee employee)
+        {
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                NumberFormatInfo nfi = new NumberFormatInfo();
+                nfi.NumberDecimalSeparator = ".";
+                MySqlCommand cmd = new MySqlCommand($"INSERT INTO employees (name, lastname, net_wage) VALUES('{employee.Name}', '{employee.LastName}', '{employee.NetWage.ToString(nfi)}')", conn);
+                
+                cmd.ExecuteScalar();
+            }
+        }
+    }
+
+    public class Employee
+    {
+        private EmployeeContext context;
+
+        public int ID { get; set; }
+
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        public decimal NetWage { get; set; }
+    }
+}
